37b4ed3309c0703b35352e89f8b8e344d058dd57
Joseph McCollum <mccolluj7506@student.faytechcc.edu>
2023-04-07 10:48:07 -0400

    Added ClassLibrary / Menu Method

--- ConsoleAppUI/Program.cs
+++ ConsoleAppUI/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GameMethodLibrary;
 
 /**
 * Feb 26 23
@@ -16,21 +17,16 @@ namespace ConsoleUI
     class Program
     {
         static void Main(string[] args)
-        {
-            int rooms = 5;
-            int weapon = 4;
-            int treasure = 3;
+        {                      
+            GameLab.Menu();
+
+            double rooms = 5;
+            double weapon = 4;
+            double treasure = 3;
             double items = 4.0;
             double mob = 5.0;
             double potions = 2.0;
-
-            Console.WriteLine("***---***---Alone In Wonderland---***---***");
-            Console.WriteLine("");
-            Console.WriteLine("Choose from the following options. ");
-            Console.WriteLine(" Main Menu \n 1. Display Rooms   \n 2. Display Weapon \n 3. Display Potion \n 4. Display Treasures \n 5. Display Items \n 6. Display Mobs \n 7. Exit");
-            Console.WriteLine("");
-            Console.WriteLine("***---------*******************---------***");
-
+            
             string menuOptions = Console.ReadLine();
 
             switch (menuOptions)
---
+++ GameMethodLibrary/GameLab.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameMethodLibrary
+{
+    public static class GameLab
+    {
+        public static void Menu()
+        {
+            Console.WriteLine("***---***---Alone In Wonderland---***---***");
+            Console.WriteLine("");
+            Console.WriteLine("Choose from the following options. ");
+            Console.WriteLine(" Main Menu \n 1. Display Rooms   \n 2. Display Weapon \n 3. Display Potion \n 4. Display Treasures \n 5. Display Items \n 6. Display Mobs \n 7. Exit");
+            Console.WriteLine("");
+            Console.WriteLine("***---------*******************---------***");
+        }
+       
+    }
+}
